6d905bb7233ff5ea02efd14afbbb82e4decbcb09
Sjotterke <thymenvds@hotmail.com>
2025-03-23 16:39:19 +0100

    go file print updates

--- main.go
+++ main.go
@@ -18,13 +18,11 @@ func main() {
 }
 
 func systemprompts_feedback(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Body)
 	// fmt.Println(r.GetBody())
 	responseData, err := io.ReadAll(r.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(responseData))
 	f, err := os.OpenFile("docs/blog/systemprompts_log.txt",
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
