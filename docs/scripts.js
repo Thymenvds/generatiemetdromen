92c7c151cbbf99d2c08b22544bf6089d2d413d57
Sjotterke <thymenvds@hotmail.com>
2025-03-23 13:44:38 +0100

    veranderingen plus feedback

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,5 +1,5 @@
-document.querySelector('.btn').addEventListener('click', function(e) {
-    e.preventDefault();
-    alert('De enquête is bijna af, kom snel terug en vul hem in!');
-  });
+// document.querySelector('.btn').addEventListener('click', function(e) {
+//     e.preventDefault();
+//     alert('De enquête is bijna af, kom snel terug en vul hem in!');
+//   });
   
--- main.go
+++ main.go
@@ -2,12 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fmt.Println("Start server")
 	fileserver := http.FileServer(http.Dir("docs"))
+	http.HandleFunc("/syspromtsfeedback", systemprompts_feedback)
 	http.Handle("/", fileserver)
 	http.ListenAndServe(":8080", nil)
+}
+
+func systemprompts_feedback(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Body)
+	// fmt.Println(r.GetBody())
+	responseData, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
+	f, err := os.OpenFile("docs/blog/systemprompts_log.txt",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer f.Close()
+	f.WriteString(string(responseData) + " \n" + "-----------------" + "\n")
 }
